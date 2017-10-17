001209968f9379c275e1645b4cef8baf6a7725af
Chase Farnsworth <cfarnswo@e1z2r5p31.42.us.org>
2017-10-16 21:26:11 -0700

    polishing - edge cases/ protections

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: cfarnswo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/06 09:24:44 by cfarnswo          #+#    #+#             */
-/*   Updated: 2017/10/06 10:51:27 by cfarnswo         ###   ########.fr       */
+/*   Updated: 2017/10/16 21:23:44 by cfarnswo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,10 @@
 
 void		ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
 {
+	if (alst == 0)
+		return ;
+	if (*alst == 0)
+		return ;
 	del(&((*alst)->content), (*alst)->content_size);
-	free(*alst);
 	*alst = NULL;
 }
